855332b5418f7a16edc3319dca32e760381f1206
LindaPermata <permatalinda531@gmail.com>
2025-11-23 19:06:32 +0700

    konfigurasi ejs

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ require('dotenv').config();
 const session = require('express-session'); // Untuk login admin
 
 const app = express();
+
+// Konfigurasi EJS sebagai template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
